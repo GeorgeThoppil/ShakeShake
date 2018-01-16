9d808c25faf8242468350ed58476504d0d7d12e5
GeorgeThoppil <georgethoppil2@gmail.com>
2018-01-15 21:00:32 -0500

    Save local username and perform segue to main game page

--- ShakeShakeApp/Controller/RegisterUsername.swift
+++ ShakeShakeApp/Controller/RegisterUsername.swift
@@ -7,10 +7,12 @@
 //
 
 import UIKit
+import Firebase
 
-class RegisterUsername: UIViewController {
+class RegisterUsername: UIViewController, UIApplicationDelegate {
     
     @IBOutlet weak var usernameInput: UITextField!
+    let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,8 +20,45 @@ class RegisterUsername: UIViewController {
         //add a bottom border and set the placeholder for the username textfield
         usernameInput.setBottomBorder()
         usernameInput.attributedPlaceholder = NSAttributedString(string: "Enter your username", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        
+        //print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
     }
-   
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+        if defaults.string(forKey: "Username") != nil  {
+            performSegue(withIdentifier: "goToHomePage", sender: self)
+        }
+    }
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        FirebaseApp.configure()
+        return true
+    }
+    
+    
+    
+    //  =========================================================
+    //  Method: registerUsername
+    //  Desc: Method that will annoymously register a username.
+    //  Args:   sender
+    //  Return: None
+    //  =========================================================
+    @IBAction func registerUsername(_ sender: Any) {
+        defaults.set(usernameInput.text!, forKey: "Username")
+        performSegue(withIdentifier: "goToHomePage", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "goToHomePage" {
+            if let destinationVC = segue.destination as? MainMenu {
+                if let userName =  defaults.string(forKey: "Username") {
+                       destinationVC.userNameFromSegue = userName
+                }
+            }
+        }
+    }
 }
 
 extension UITextField {
